bf5110a9297d16270f93a1ca7bd522448d7ef5b1
Rob Gordon <rchristopher.gordon@gmail.com>
2019-11-09 10:31:50 -0500

    feat: update action and fix error

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,5 @@
 export { default as Reset } from './Reset/Reset';
 export { default as Box } from './Box/Box';
-export { default as Grow } from './Grow/Grow';
 export { default as Container } from './Container/Container';
 export { default as List } from './List/List';
 export { default as Group } from './Group/Group';
