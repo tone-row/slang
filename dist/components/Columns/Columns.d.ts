e30ad70cce4518b324b2ade0140695f9e2378b0c
Rob Gordon <rchristopher.gordon@gmail.com>
2019-11-10 11:32:38 -0500

    feat: added examples

--- dist/components/Columns/Columns.d.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-declare type StyledColumnsProps = {
-  gutter?: string;
-};
-interface ColumnsProps extends StyledColumnsProps {
-  itemProps?: any;
-}
-declare const Columns: React.FC<ColumnsProps>;
-export default Columns;
--- dist/components/Grow/Grow.d.ts
+++
@@ -1,2 +0,0 @@
-declare const Grow: import("styled-components").StyledComponent<"div", any, import("../Box/Box").BoxProps, never>;
-export default Grow;
