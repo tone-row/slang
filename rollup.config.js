298779c0a6f3dea2e0fc03827e9490eea8519ddc
Rob Gordon <rchristopher.gordon@gmail.com>
2019-11-17 22:54:55 -0500

    build: tree shaking improvements

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,15 @@
+import { readdirSync } from 'fs';
+import { join } from 'path';
 import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
 import pkg from './package.json';
+
+const componentsDir = './src/components';
+const components = readdirSync(componentsDir, { withFileTypes: true })
+  .filter(dir => dir.isDirectory())
+  .map(dir => join(componentsDir, dir.name, `${dir.name}.tsx`));
 
 const extensions = ['.js', '.jsx', '.ts', '.tsx'];
 
@@ -13,9 +20,12 @@ const globals = {
 };
 
 export default {
-  input: 'src/index.tsx',
+  input: ['src/index.tsx'],
   external: Object.keys(pkg.peerDependencies || {}),
-  output: [{ file: pkg.main, format: 'cjs', globals }, { file: pkg.module, format: 'esm', globals }],
+  output: [
+    { dir: 'dist', entryFileNames: '[name].js', format: 'cjs', globals },
+    { dir: 'dist', entryFileNames: '[name].module.js', format: 'esm', globals },
+  ],
   plugins: [
     resolve({ extensions }),
     commonjs(),
