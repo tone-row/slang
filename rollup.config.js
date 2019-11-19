6c29c8663375d860c09f3fbb97177a54b63213fd
Rob Gordon <rchristopher.gordon@gmail.com>
2019-11-19 17:33:08 -0500

    feat: switch to yarn

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,8 @@
-import { readdirSync } from 'fs';
-import { join } from 'path';
 import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
 import pkg from './package.json';
-
-const componentsDir = './src/components';
-const components = readdirSync(componentsDir, { withFileTypes: true })
-  .filter(dir => dir.isDirectory())
-  .map(dir => join(componentsDir, dir.name, `${dir.name}.tsx`));
 
 const extensions = ['.js', '.jsx', '.ts', '.tsx'];
 
